9f0e5d971edc02259d6f4872a8fb63bb4e04a3b8
Aditya <63795831+Aditya231@users.noreply.github.com>
2020-10-01 10:05:06 +0530

    simple change

--- template.php
+++ template.php
@@ -6,6 +6,8 @@
         <link href="/css/index.css" rel="stylesheet">
     </head>
     <body>
+        <h1 style="text-align: center; align-items: center;><b><u>Welcome to The Basic Php Db Tuts Page !</h1></u></b>
+     <!--Giving A heading improves the simple basic structure of any website !-->
 
         <table>
 
