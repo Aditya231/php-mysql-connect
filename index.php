6b24b3f2f089dafd24cf187e46ed1fcb6f2aaf1a
Aditya <63795831+Aditya231@users.noreply.github.com>
2020-10-01 09:59:39 +0530

    updated docs , comments for beginners !

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
 $db = new PDO('mysql:host=localhost;dbname=testdb;charset=utf8', 'username', 'password');
-
+//else you can also use 'MySQL' as follows :
+/*
+$host = "localhost"; //servername here
+$user = "root"; //default user in xampp, localhost
+$pass = ""; //in xampp password is blank / nothing
+$db = "testdb"; //your db name here
+$conn = mysqli_connect($host, $user, $pass, $db);
+*/
+// Try Using Sql ! Its Good For Database Connection !
 include 'IndexModel.php';
 
 $indexModel = new IndexModel($db);
 $result = $indexModel->getTheData();
 
 include 'template.php';
+?>
